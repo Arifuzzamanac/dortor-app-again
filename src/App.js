df36e4f0e2b071c5119ab8abefda81fffed25c68
Arifuzzamanac <arifac653@gmail.com>
2021-11-17 13:24:33 +0600

    again commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from './Pages/Home/Home/Home';
 
 function App() {
   return (
-    <div>
-      doctor ready ho
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/about">
+          <Home />
+        </Route>
+        <Route path="/users">
+          <Home />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ text.js
@@ -0,0 +1,3 @@
+git add .
+git commit -m"again commit"
+git push 
